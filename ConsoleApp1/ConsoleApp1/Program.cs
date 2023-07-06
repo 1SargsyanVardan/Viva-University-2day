582055fb59d1a578007b85ef2f6362ae9677ad5b
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-06 23:34:19 +0400

    Task 1: Delegate Invocation

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string str = "Barev";
+PrintDelegate print;
+print = Barev;
+print(str);
+print.Invoke(str);
+void Barev(string str) => Console.WriteLine(str);
+delegate void PrintDelegate(string str);
