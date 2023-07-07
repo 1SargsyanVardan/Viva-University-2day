bd2c117417fb3ff668877608abbc4637518f86c3
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-07 09:32:04 +0400

    Task 3: Anonymous Method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,4 +11,22 @@ delegate void PrintDelegate(string str);*/
 
 //------------------------Task 2------------------------------------
 
-ConsoleReadEvent isClicked = new ConsoleReadEvent("clicked");
+/*ConsoleReadEvent isClicked = new ConsoleReadEvent("clicked");*/
+
+//------------------------Task 3------------------------------------
+
+int a = 10;
+int b = 20;
+Operation add = delegate (int x, int y)
+{
+    return x + y;
+};
+Operation sub = delegate (int x, int y)
+{
+    return a - b;
+};
+int addResult = add(a, b);
+int subResult = sub(a, b);
+Console.WriteLine($"{a} + {b} = {addResult}");
+Console.WriteLine($"{a} - {b} = {subResult}");
+delegate int Operation(int x, int y);
