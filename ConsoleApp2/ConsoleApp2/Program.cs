30115fee1fe5130950cbb863f5c8ae7fb8a67e60
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-09 22:44:12 +0400

    Task 1 changed(DateTime)

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,9 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-DateTime date = DateTime.Now;
-date = date.AddDays(30);
-
-string patet = "Dzer sakagnayin planum nerarvats patetnery veraaktivacel en yev gortsum en minchev " + date.ToString("MM/dd/yyyy") + 
-    "\nPatetneri mnacordy stugelu hamar ugharkeq *208#.";
-Console.WriteLine(patet);
+﻿DateTime date = DateTime.Now;
+Console.WriteLine(date);
+ReplaceDate(ref date);
+Patetner(date);
+void ReplaceDate(ref DateTime date)
+{
+    date = date.AddDays(30);
+}
+void Patetner(DateTime date)
+{
+    Console.WriteLine("Dzer sakagnayin planum nerarvats patetnery veraaktivacel en yev gortsum en minchev " + date.Day + "/" + date.Month + "/" +date.Year + 
+        " "  + date.Hour + ":" + date.Minute);
+    Console.WriteLine("Patetneri mnacordy stugelu hamar ugharkeq *208#.");
+}
