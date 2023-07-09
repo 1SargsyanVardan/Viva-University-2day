7e15284e89e9974c32ddf664f88ac8ffe0d85959
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-09 22:50:47 +0400

    Task 3(dateTime)

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿DateTime date = DateTime.Now;
+﻿//----------Task1
+
+/*DateTime date = DateTime.Now;
 Console.WriteLine(date);
 ReplaceDate(ref date);
 Patetner(date);
@@ -11,4 +13,17 @@ void Patetner(DateTime date)
     Console.WriteLine("Dzer sakagnayin planum nerarvats patetnery veraaktivacel en yev gortsum en minchev " + date.Day + "/" + date.Month + "/" +date.Year + 
         " "  + date.Hour + ":" + date.Minute);
     Console.WriteLine("Patetneri mnacordy stugelu hamar ugharkeq *208#.");
+}*/
+
+//----------Task3
+DateTime datetime1 = new DateTime(2023, 6, 30);
+DateTime datetime2 = new DateTime(2023, 7, 8);
+
+int elapsedDays = ElapsedDays(datetime1, datetime2);
+Console.WriteLine("Elapsed days: " + elapsedDays);
+int ElapsedDays(DateTime datetime1, DateTime datetime2)
+{
+    TimeSpan timespan = datetime2 - datetime1;
+    int elapsedDays = (int)timespan.TotalDays;
+    return elapsedDays;
 }
