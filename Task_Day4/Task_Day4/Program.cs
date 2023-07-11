762a23e7a2c82dfd105f6c589409ed7f2d00ad35
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-11 10:47:21 +0400

    Task 1: File Processing with Exception Handling ((CHANGED))

--- Task_Day4/Task_Day4/Program.cs
+++ Task_Day4/Task_Day4/Program.cs
@@ -2,29 +2,20 @@
 try
 {
     string str = File.ReadAllText(@"C:\Users\User\OneDrive\Рабочий стол\Viva-University-2day\Task_Day4\MyFirstFile.txt");
-    string newStr = str.AllUpperCase();
+    string newStr = str.ToUpper();
     File.WriteAllText(@"C:\Users\User\OneDrive\Рабочий стол\Viva-University-2day\Task_Day4\MySecondFile.txt", newStr);
 }
-catch
+catch(FileNotFoundException e)
 {
-    Console.WriteLine("File isn`t found");
+    //Console.WriteLine("File isn`t found");
+    Console.WriteLine(e.Message);
 }
+catch(IOException)
+{
+    Console.WriteLine("IOException");
+}
+
 finally
 {
     Console.WriteLine("Finally block");
-}
-public static class StringExtension
-{
-    public static string AllUpperCase(this string str)
-    {
-        string[] strArray = str.Split(' ');
-        for (int i = 0; i < strArray.Length; i++)
-        {
-            char ch = char.ToUpper(strArray[i][0]);
-            string otherStr = strArray[i].Substring(1).ToLower();
-            strArray[i] = ch + otherStr;
-        }
-        
-        return string.Join(' ', strArray);
-    }
 }
