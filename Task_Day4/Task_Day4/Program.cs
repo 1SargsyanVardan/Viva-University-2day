58b440b42406ec16311a75e2613d5a63e6a029cc
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-11 11:10:45 +0400

    Task 2: Input Validation with Exception Handling (2 versia)

--- Task_Day4/Task_Day4/Program.cs
+++ Task_Day4/Task_Day4/Program.cs
@@ -1,5 +1,5 @@
 ﻿//-------------------------Task 1----------------------
-try
+/*try
 {
     string str = File.ReadAllText(@"C:\Users\User\OneDrive\Рабочий стол\Viva-University-2day\Task_Day4\MyFirstFile.txt");
     string newStr = str.ToUpper();
@@ -18,4 +18,62 @@ catch(IOException)
 finally
 {
     Console.WriteLine("Finally block");
+}*/
+
+//-------------------------Task 2----------------------
+
+
+//first versia-------------------------------------
+
+/*
+Console.Write("(If your age between 18 and 40) Please input your age: ");
+string? inputString = Console.ReadLine();
+
+int age;
+if (int.TryParse(inputString, out _))
+{
+    age = int.Parse(inputString);
+    try
+    {
+        if (age < 18 || age > 40)
+        {
+            throw new ArgumentException("Age must be between 18 and 40.");
+        }
+        Console.WriteLine("Valided");
+
+    }
+    catch (ArgumentException e)
+
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+else
+    Console.WriteLine("Input value is`t number");*/
+
+//second versia-------------------------------------
+while (true)
+{
+    Console.Write("(If your age between 18 and 40) Please input your age: ");
+    string? inputString = Console.ReadLine();
+
+    int age;
+    try
+    {
+        age = int.Parse(inputString);
+        if (age < 18 || age > 40)
+        {
+            throw new ArgumentException("Age must be between 18 and 40.");
+        }
+        Console.WriteLine("Valided!");
+        break;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine(e.Message);
+    }
 }
