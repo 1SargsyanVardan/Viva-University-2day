c9284d15b265bc3ebc471bcbcfa14d3d70013d5a
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-17 12:48:58 +0400

    Task 3 Async/Await

--- Tasks_Day5/Tasks_Day5/Program.cs
+++ Tasks_Day5/Tasks_Day5/Program.cs
@@ -45,14 +45,14 @@ Console.WriteLine("All lines in two files = " + allLines);*/
 
 //--------------------------------------------Task 2--------------------------
 //kargavorvac amboxj directoriay hamar
-using System;
+/*using System;
 using System.Drawing;
 using System.IO;
 using System.Drawing.Imaging;
 
 
 string directory = @"C:\Users\User\OneDrive\Рабочий стол\Viva-University-2day\Tasks_Day5";
-string[] imageInDirectory = Directory.GetFiles(directory, "*jpeg");
+string[] imageInDirectory = Directory.GetFiles(directory, "*jpeg");*/
 
 //myFunc(imageInDirectory);
 //var stringPattern = new[] { "*jpeg", "*png", "*svg", "*WebP" };
@@ -62,7 +62,7 @@ string[] imageInDirectory = Directory.GetFiles(directory, "*jpeg");
 {
     Console.WriteLine(image);
 }*/
-Task t = myFunc(imageInDirectory);
+/*Task t = myFunc(imageInDirectory);
 t.Wait();
 static Bitmap SetGrayscale(Bitmap btmap)
 {
@@ -104,7 +104,7 @@ Task Func(string[] imageInDirectory)
 
         }
     });
-}
+}*/
 /*Image image = Image.FromStream(File.OpenRead(@"C:\Users\User\OneDrive\Рабочий стол\Viva-University-2day\Tasks_Day5\Viva.jpeg"));
 Console.WriteLine(image.Height);
 Console.WriteLine(image.PixelFormat);*/
@@ -126,3 +126,81 @@ Console.WriteLine(image.PixelFormat);*/
 
 
 //--------------------------------------------Task 3-------------------------------------
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<Action> actions = new List<Action>();
+        Action printMessage = () => Console.WriteLine("Hello world");
+        Action printName = () => Console.WriteLine("My name is Vardan");
+        Action printOld = () => Console.WriteLine("I`m 22 years old");
+        Action printCountry = () => Console.WriteLine("I`m from Armenia");
+        actions.Add(printMessage);
+        actions.Add(printName);
+        actions.Add(printCountry);
+        actions.Add(printOld);
+        myFunctionsList func = new myFunctionsList(actions);
+        DateTime d = DateTime.Now.AddSeconds(5);
+        /*Console.WriteLine("------------------RunTime-------------------");
+        await func.runTime(2000, 8000);*/
+        Console.WriteLine("------------------RunDateTime----------------");
+        await func.runDateTime(d, 10000);
+    }
+}
+    
+
+class myFunctionsList
+{
+    List<Action> functionsList = new List<Action>();
+    CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+    public myFunctionsList()
+    {
+
+    }
+    
+    public myFunctionsList(List<Action> functionsList)
+    {
+        this.functionsList = functionsList;
+    }
+    public async Task runTime(int interval,int allcanceled)
+    {
+        Console.WriteLine("async func started");
+        cancelTokenSource.CancelAfter(allcanceled);
+        while (!cancelTokenSource.Token.IsCancellationRequested)
+        {
+            foreach (var item in functionsList)
+            {
+                item();
+            }
+            await Wait(interval);
+            //await Task.Delay(interval);
+
+
+        }
+        Console.WriteLine("async func ended");
+    }
+    public async Task runDateTime(DateTime dateTime, int allcanceled)
+    {
+        Console.WriteLine("async func started");
+        await Task.Delay(dateTime - DateTime.Now);
+
+        cancelTokenSource.CancelAfter(allcanceled);
+        if (!cancelTokenSource.Token.IsCancellationRequested)
+        {
+            foreach (var item in functionsList)
+            {
+                item();
+            }
+        }
+        Console.WriteLine("async func ended");
+    }
+    private static Task Wait(int interval)
+    {
+        return Task.Run (async () =>
+        {
+            await Task.Delay(interval);
+            Console.WriteLine($"{interval} Seconds wait Completed\n");
+        });
+            
+    }
+}
